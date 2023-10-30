bb9988723e40bcbace90c41e5901e9d11e172411
Ruchita613 <ruchita.sawale@getpeppermint.co>
2023-10-30 19:19:54 +0530

    tic - tac - toe

---
+++ lib/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static Color primaryColor = Color.fromARGB(255, 240, 126, 217);
+  static Color secondaryColor = Color.fromARGB(255, 30, 11, 238);
+  static Color accentColor = Color.fromARGB(255, 183, 0, 255);
+}
